c945c70da572281d95068ecdd1a2c8cdb8a4ef8b
Fady1900456 <fady1900456@miuegypt.edu.eg>
2023-01-02 02:16:08 +0200

    code upload

--- lib/presentation/search_screen.dart
+++ lib/presentation/search_screen.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:network_image_search/network_image_search.dart';
+import 'package:search_page/search_page.dart';
 
+import '../model/post.dart';
+import '../service/posts_service.dart';
 import 'app_scaffold.dart';
 
 class SearchScreen extends StatefulWidget {
@@ -10,10 +15,62 @@ class SearchScreen extends StatefulWidget {
 }
 
 class _SearchScreenState extends State<SearchScreen> {
+  List<Post> posts = <Post>[];
+  TextEditingController editingController = TextEditingController();
+
+  var isLoaded = false;
+
+  getData() async {
+    posts = await PostsService().getUniversities();
+
+    if (posts.isNotEmpty) {
+      setState(() {
+        isLoaded = true;
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    getData();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return AppScaffold(
-      child: Container(),
+      child: Scaffold(
+        body: Visibility(
+            visible: isLoaded,
+            replacement: const Center(child: CircularProgressIndicator()),
+            child: ListView.builder(
+                itemCount: posts.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(posts[index].name.toString()),
+                  );
+                })),
+        floatingActionButton: FloatingActionButton(
+          tooltip: 'Search people',
+          onPressed: () => showSearch(
+            context: context,
+            delegate: SearchPage(
+              items: posts,
+              searchLabel: 'Search University',
+              failure: const Center(
+                child: Text('No matches'),
+              ),
+              filter: (university) => [
+                university.name.toString(),
+              ],
+              builder: (university) => ListTile(
+                title: Text(university.name.toString()),
+              ),
+            ),
+          ),
+          child: const Icon(Icons.search),
+        ),
+      ),
     );
   }
 }
